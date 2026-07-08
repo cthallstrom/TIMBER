d930874a8e7cf8b44e403857e971af689bdeb894
Hannah Larson <hlarson@cmslpc374.fnal.gov>
2026-07-08 15:03:06 -0500

    fixed parentheses issue and removed hardcoded t tree proccesing

--- bin/restframes/helper.cc
+++ bin/restframes/helper.cc
@@ -3,18 +3,7 @@
 #include <iostream>
 
 RVec<double> processDecayTree(Tprime_RestFrames_Container_W * W_rfc, Tprime_RestFrames_Container_t * t_rfc, int thread_index, float lepton_pt, float lepton_eta, float lepton_phi, float lepton_mass, RVec<float> fatjet_pt, RVec<float> fatjet_eta, RVec<float> fatjet_phi, RVec<float> fatjet_mass, float met_pt, float met_phi, RVec<TLorentzVector> bjets, TLorentzVector minMlb_lv, int decayMode) {
-  RVec<TLorentzVector> jets;
-  int i = 0; 
 
-  // Make an RVec of valid jets for the possible b.
-  // for (; i < isoAK4.size(); i++) {
-  //   //  stand alone         b-tagged
-  //   if (jet_BTag[i] == 1) {
-  //     jets.push_back(bjets[i]);
-  //   } 
-  // }   
-
-  //  std::cout << "-------------------------------------------" << std::endl;
   RVec<double> result;
   if (decayMode == 0) { // analyze bW tree
     result = W_rfc->return_W_doubles(thread_index, lepton_pt, lepton_eta, lepton_phi, lepton_mass, fatjet_pt, fatjet_eta, fatjet_phi, fatjet_mass, met_pt, met_phi);
@@ -80,22 +69,26 @@ int R_decayTypes(int lepton_source, int J0_idx, int VLQ21_idx, int VLQ22_idx, RV
       if(PNWMtags.at(VLQ22_idx) == 25 || PNWMtags.at(VLQ22_idx) == 25) PNWM_ID = 6; //bW tH
       if(PNWMtags.at(VLQ22_idx) == 23 || PNWMtags.at(VLQ22_idx) == 23) PNWM_ID = 5; //bW tZ
     }
-    else if ((PNWMtags.at(VLQ21_idx) == 5 || PNWMtags.at(VLQ22_idx) == 5) && PNWMtags.at(VLQ21_idx) == 24 || PNWMtags.at(VLQ22_idx) == 24) PNWM_ID = 1; //bW bW
+    else if (PNWMtags.at(VLQ21_idx) == 5 || PNWMtags.at(VLQ22_idx) == 5) {
+      if (PNWMtags.at(VLQ21_idx) == 24 || PNWMtags.at(VLQ22_idx) == 24) PNWM_ID = 1; //bW bW
+    }
   }
 
   else if (lepton_source == 1) { //t? ??
     if (PNWMtags.at(J0_idx) == 23) { //tZ ??
       if (PNWMtags.at(VLQ21_idx) == 0 || PNWMtags.at(VLQ22_idx) == 0) PNWM_ID = 8; //medium purity, tZ
-    
+      
       else if(PNWMtags.at(VLQ21_idx) == 6 || PNWMtags.at(VLQ22_idx) == 6) { //tZ t?
 	if(PNWMtags.at(VLQ22_idx) == 25 || PNWMtags.at(VLQ22_idx) == 25) PNWM_ID = 4; //tZ tH
 	if(PNWMtags.at(VLQ22_idx) == 23 || PNWMtags.at(VLQ22_idx) == 23) PNWM_ID = 2; //tZ tZ
       }
-      else if ((PNWMtags.at(VLQ21_idx) == 5 || PNWMtags.at(VLQ22_idx) == 5) && PNWMtags.at(VLQ21_idx) == 24 || PNWMtags.at(VLQ22_idx) == 24) PNWM_ID = 5; //tZ bW
+      else if (PNWMtags.at(VLQ21_idx) == 5 || PNWMtags.at(VLQ22_idx) == 5) {
+	if (PNWMtags.at(VLQ21_idx) == 24 || PNWMtags.at(VLQ22_idx) == 24) PNWM_ID = 5; //tZ bW
+      }
     }
     else if (PNWMtags.at(J0_idx) == 25) { //tH ??
       if (PNWMtags.at(VLQ21_idx) == 0 || PNWMtags.at(VLQ22_idx) == 0) PNWM_ID = 7; //medium purity, tH
-    
+      
       else if(PNWMtags.at(VLQ21_idx) == 6 || PNWMtags.at(VLQ22_idx) == 6) { //tH t?
 	if(PNWMtags.at(VLQ22_idx) == 25 || PNWMtags.at(VLQ22_idx) == 25) PNWM_ID = 4; //tH tZ
 	if(PNWMtags.at(VLQ22_idx) == 23 || PNWMtags.at(VLQ22_idx) == 23) PNWM_ID = 3; //tH tH
